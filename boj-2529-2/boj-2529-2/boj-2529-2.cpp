a4842c5af8782ee5c7676ce928fe2eface682fab
sts08015 <sts08015@gmail.com>
2021-11-05 21:57:54 +0900

    boj

---
+++ boj-2529-2/boj-2529-2/boj-2529-2.cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int indegree1[10];
+int indegree2[10];
+
+int main(void)
+{
+    int k;
+    scanf("%d", &k);
+    char* sign = (char*)malloc(sizeof(char) * k);
+    vector<int> arr[10];
+
+    for (int i = 0; i < k; i++)
+    {
+        getchar();
+        scanf("%c", sign + i);
+        
+        if (sign[i] == '<')
+        {
+            indegree1[i + 1]++;
+            indegree2[i + 1]++;
+            arr[i].push_back(i + 1);
+        }
+        else
+        {
+            indegree1[i]++;
+            indegree2[i]++;
+            arr[i + 1].push_back(i);
+        }
+    }
+
+    priority_queue<int> max_heap;
+    priority_queue<int,vector<int>,greater<int>> min_heap;
+
+    for (int i = 0; i < k + 1; i++)
+    {
+        if (indegree1[i] == 0)
+        {
+            max_heap.push(i);
+            min_heap.push(i);
+        }
+    }
+
+    char* ans1 = (char*)malloc(sizeof(char) * 10);
+    char* ans2 = (char*)malloc(sizeof(char) * 10);
+
+    int cnt = 0;
+    for (int i = 0; i < k + 1; i++)
+    {
+        int tmp = max_heap.top();
+        max_heap.pop();
+        ans1[tmp] = cnt++;
+        for (int j = 0; j < arr[tmp].size(); j++)
+        {
+            int x = arr[tmp][j];
+            if (--indegree1[x] == 0) max_heap.push(x);
+        }
+    }
+    cnt = 0;
+    for (int i = 0; i < k + 1; i++)
+    {
+        int tmp = min_heap.top();
+        min_heap.pop();
+        ans2[tmp] = cnt++;
+        for (int j = 0; j < arr[tmp].size(); j++)
+        {
+            int x = arr[tmp][j];
+            if (--indegree2[x] == 0) min_heap.push(x);
+        }
+    }
+    for (int i = 0; i < k + 1; i++) printf("%d", ans1[i] + 9 - k);
+    puts("");
+    for (int i = 0; i < k + 1; i++) printf("%d", ans2[i]);
+    
+    free(ans1);
+    free(ans2);
+    free(sign);
+    return 0;
+}
+
---
+++ boj-2529/boj-2529/boj-2529.cpp
@@ -0,0 +1,91 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef enum
+{
+	MAX, MIN
+}MODE;
+
+char chk[10];
+
+bool solve(char* arr,char* brr,int len,int depth,MODE mode)
+{
+	if (len+1 == depth)
+	{
+		return true;
+	}
+
+	bool find = false;
+	for (int i = 9; i >= 0; i--)
+	{
+		int j;
+		if (mode == MIN) j = 9 - i;
+		else j = i;
+
+		if (!chk[j])
+		{
+			if (depth == 0)
+			{
+				if (j == 0 && arr[depth] == '>') continue;
+				else if (j == 9 && arr[depth] == '<') continue;
+			}
+			else
+			{
+				if (depth == len)
+				{
+					if (j == 0 && arr[depth] == '<') continue;
+					else if (j == 9 && arr[depth] == '>') continue;
+				}
+				if (arr[depth-1] == '<')
+				{
+					if (brr[depth - 1] > j) continue;
+				}
+				else
+				{
+					if (brr[depth - 1] < j) continue;
+				}
+			}
+			brr[depth] = j;
+			chk[j]++;
+			find = true;
+			bool val = solve(arr, brr, len, depth + 1,mode);
+			if (!val)
+			{
+				chk[brr[depth]]--;
+				brr[depth] = 0;
+				find = false;
+			}
+			else break;
+		}
+	}
+
+	return find;
+}
+int main(void)
+{
+	int k;
+	scanf("%d", &k);
+	char* arr = (char*)malloc(sizeof(char) * k);
+	char* brr = (char*)calloc(k + 1,sizeof(char));
+	char* crr = (char*)calloc(k + 1, sizeof(char));
+
+	for (int i = 0; i < k; i++)
+	{
+		getchar();
+		scanf("%c", arr + i);
+	}
+
+	solve(arr, brr, k, 0,MAX);
+	for (int i = 0; i < k + 1; i++) printf("%d", brr[i]);
+	puts("");
+	memset(chk, 0, 10);
+	solve(arr, crr, k, 0,MIN);
+	for (int i = 0; i < k + 1; i++) printf("%d", crr[i]);
+
+	free(arr);
+	free(brr);
+	free(crr);
+	return 0;
+}
